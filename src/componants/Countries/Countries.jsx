9ca3394600ef3fdcdcd846a764d58aa08fa6a78b
parvezpatwaryy <tanjilapatwaryy@gmail.com>
2026-03-18 13:04:20 +0600

    thrid commit

--- src/componants/Countries/Countries.jsx
+++ src/componants/Countries/Countries.jsx
@@ -1,18 +1,22 @@
 import React, { use } from 'react';
 import Country from '../Country/Country';
+import './Countries.css'
 
-const Countries = ({countriesPromise}) => {
+
+const Countries = ({ countriesPromise }) => {
   const countriesData = use(countriesPromise)
   const countries = countriesData.countries
-  
+
   return (
     <div>
       <h1>In the countries:{countries.length}</h1>
-      {
-       countries.map(country =><Country 
-        key ={country.cca3.cca3}
-        country={country}></Country>) 
-      }
+      <div className='countries'>
+        {
+          countries.map(country => <Country
+            key={country.cca3.cca3npm}
+            country={country}></Country>)
+        }
+      </div>
     </div>
   );
 };
--- src/componants/Country/Country.jsx
+++ src/componants/Country/Country.jsx
@@ -1,20 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './Country.css'
+const Country = ({ country }) => {
 
-const Country = ({country}) => {
-  console.log(country.region.region)
+  const [visited ,setVisited] = useState(false)
+  // console.log(country.region.region)
+
+  const handleVisited = () => {
+
+    
+    // nume number = 3
+    setVisited(!visited)
+
+
+    // nume number = 2
+    // setVisited(visited? false : true)
+
+
+
+    // nume number = 1
+    // if (visited){
+    //   setVisited(false)
+    // }
+    // else{
+    //   setVisited(true)
+    // }
+  }
+
   return (
-    <div>
+    <div className={`country ${visited && 'country-visited'}`}>
       <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
       <h1>Name:{country.name.common}</h1>
       <h3>OFFICIAL NAME:{country.name.official}</h3>
-      <h3>AREA:{country.area.area}</h3>
+      <h3>AREA:{country.area.area}{country.area.area > 300000 ? 'Big Country' : 'Small Country'}</h3>
       <h4>CAPITAL:{country.capital.capital}</h4>
       <h4>CCA3:{country.cca3.cca3}</h4>
       <h4>CCN3:{country.ccn3.ccn3}</h4>
       <h4>CONTINENTS:{country.continents.continents}</h4>
       <h4>REGION:{country.region.region}</h4>
       <h4>POPULATION:{country.population.population}</h4>
-      
+      <button onClick={handleVisited} >{visited ? 'Visited':'Not visited'}</button>
     </div>
   );
 };
